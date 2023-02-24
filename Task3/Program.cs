597ebafac64f0231558aaf4186591d38af3cf842
LexLuter <gae_cbr@mail.ru>
2023-02-24 19:35:40 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+var N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    var cube = Math.Pow(i, 3);
+    if (i == N) Console.Write($"{cube}."); 
+    else Console.Write($"{cube}, ");
+}
+
+
+
