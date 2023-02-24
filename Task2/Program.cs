66417ec6680a8ea220462ebdd7c1fa366850fc07
LexLuter <gae_cbr@mail.ru>
2023-02-24 18:57:38 +0700

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите координаты точки А по оси Х: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки А по оси Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки А по оси Z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки B по оси Х: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки B по оси Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки B по оси Z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+Console.WriteLine($"Расстояние между координатами равно: {distance}.");
